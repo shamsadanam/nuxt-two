edfa6cc8b0518749598d7d22af17b9968a51baf8
shamsadanam <shamsad.anam@gmail.com>
2023-03-09 17:11:26 +0600

    fc

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <input type="text" v-model="userId" />
+    <button @click="handleLoadUser">Load User</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "IndexPage",
+  data() {
+    return {
+      userId: "",
+    };
+  },
+  methods: {
+    handleLoadUser() {
+      console.log(`clicked load user : ${this.userId}`);
+      this.$router.push(`/user/${this.userId}`);
+    },
+  },
+};
+</script>
---
+++ pages/user/_id.vue
@@ -0,0 +1,4 @@
+<template>
+  <h1>This is user {{ $route.params.id }}</h1>
+</template>
+<script></script>
